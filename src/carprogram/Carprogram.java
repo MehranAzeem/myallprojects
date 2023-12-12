ff3ef613577e281ecb0bd3591e39c11439f3f0d2
MehranAzeem <153623232+MehranAzeem@users.noreply.github.com>
2023-12-12 13:53:37 -0800

    Add files via upload

---
+++ src/carprogram/Carprogram.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carprogram;
+
+ import java.util.Scanner;
+ 
+ 
+public class Carprogram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("enter the maxi no");
+           
+           int num1 =sc.nextInt();
+           
+          int num2 =sc.nextInt();
+           
+           if (num1>num2){
+        System.out.println(num1 +  "is maxi no");
+    }
+           else{           System.out.println(num2 + " is large no");
+          
+           
+           
+       
+       
+    
+       
+       
+        // TODO code application logic here
+    }
+    }
+}
