97b795d19992381be1d85e9141b7f5a3f9b1e1e7
MehranAzeem <153623232+MehranAzeem@users.noreply.github.com>
2023-12-12 13:54:34 -0800

    Add files via upload

---
+++ atmprogram/CallMethod.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class CallMethod{
+
+public static void main(String[] arg){
+
+
+  Scanner sc=new Scanner(System.in);
+   
+   Atmprogram ap=new Atmprogram ();
+   ap.displayatm();
+   
+
+}
+
+}
